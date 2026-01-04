771f6f8163f3f54e42be4250044fa69c3fa7853e
Anduin <anduin@aiursoft.com>
2026-01-04 15:53:19 +0000

    fix: Add docker-compose-plugin to package installation in ensure_packages_needed_ready function

--- deploy.sh
+++ deploy.sh
@@ -99,7 +99,7 @@ function ensure_docker_ready() {
 }
 
 function ensure_packages_needed_ready() {
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y wsdd valgrind curl wget apt-transport-https ca-certificates
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y wsdd valgrind curl wget apt-transport-https ca-certificates docker-compose-plugin
 
     # Install yq. (Run install_yq only if the /usr/bin/yq does not exist.)
     [ -f /usr/bin/yq ] || install_yq
